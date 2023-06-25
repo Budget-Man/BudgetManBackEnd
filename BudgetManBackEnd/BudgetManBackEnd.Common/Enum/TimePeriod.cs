a91f87f3680a19937d0b78d5550b5b92a194b03b
Phan Thanh Binh <maynghien@hotmail.com>
2023-06-25 20:24:03 +0700

    change class

---
+++ BudgetManBackEnd/BudgetManBackEnd.Common/Enum/TimePeriod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManBackEnd.CommonClass.Enum
+{
+    public enum TimePeriod
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        Quarterly,
+        Annually
+    }
+}
---
+++ BudgetManBackEnd/BudgetManBackEnd.DAL/Models/Entity/Debts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManBackEnd.DAL.Models.Entity
+{
+    public class Debts:BaseAccountEntity
+    {
+        public string Name { get; set; }
+        public double TotalAmount { get; set; }
+        public double? RemainAmount { get; set; }
+        public double? PaidAmount { get; set; }
+        public double? TotalInterest { get; set; }
+        public double InterestRate { get; set; } = 0;
+        public string RatePeriod { get; set; }
+    }
+}
