ae5cf4643323a9dbca8b15c470ea4568b648173e
binh.phan@s3corp.com.vn <binh.phan@s3corp.com.vn>
2023-06-30 16:54:41 +0700

    service

---
+++ BudgetManBackEnd/BudgetManBackEnd.Service/Contract/IBudgetCategoryService.cs
@@ -0,0 +1,14 @@
+﻿using BudgetManBackEnd.DAL.Models.Entity;
+using BudgetManBackEnd.Model.Dto;
+using MayNghien.Models.Response.Base;
+
+namespace BudgetManBackEnd.Service.Contract
+{
+    public interface IBudgetCategoryService
+    {
+        AppResponse<List<BudgetCategoryDto>> GetAllBudgetCategory(string userId);
+        AppResponse<BudgetCategoryDto> CreatebudgetCategory(BudgetCategoryDto budgetCategory);
+        AppResponse<BudgetCategoryDto> EditbudgetCategory(BudgetCategoryDto budgetCategory);
+        AppResponse<string> DeletebudgetCategory(Guid budgetCategoryId);
+    }
+}
---
+++ BudgetManBackEnd/BudgetManBackEnd.Service/Implementation/BudgetCategoryService.cs
@@ -0,0 +1,30 @@
+﻿using BudgetManBackEnd.Model.Dto;
+using BudgetManBackEnd.Service.Contract;
+using MayNghien.Models.Response.Base;
+
+namespace BudgetManBackEnd.Service.Implementation
+{
+    public class BudgetCategoryService : IBudgetCategoryService
+    {
+        private readonly IBudgetCategoryRepository _budgetCategoryRepository;
+        public AppResponse<BudgetCategoryDto> CreatebudgetCategory(BudgetCategoryDto budgetCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppResponse<string> DeletebudgetCategory(Guid budgetCategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppResponse<BudgetCategoryDto> EditbudgetCategory(BudgetCategoryDto budgetCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppResponse<List<BudgetCategoryDto>> GetAllBudgetCategory(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
