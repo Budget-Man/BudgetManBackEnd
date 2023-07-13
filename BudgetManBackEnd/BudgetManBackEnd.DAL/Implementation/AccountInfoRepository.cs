f0f4f8f0ef0fe8f53ceb2ab237dd5d03a1e32583
binh.phan@s3corp.com.vn <binh.phan@s3corp.com.vn>
2023-07-13 18:58:27 +0700

    add AccountInfoRepository

---
+++ BudgetManBackEnd/BudgetManBackEnd.DAL/Implementation/AccountInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using BudgetManBackEnd.DAL.Contract;
+using BudgetManBackEnd.DAL.Models.Context;
+using BudgetManBackEnd.DAL.Models.Entity;
+using Maynghien.Common.Repository;
+
+namespace BudgetManBackEnd.DAL.Implementation
+{
+    internal class AccountInfoRepository : GenericRepository<AccountInfo, BudgetManDbContext>, IAccountInfoRepository
+    {
+        public AccountInfoRepository(BudgetManDbContext unitOfWork) : base(unitOfWork)
+        {
+            _context = unitOfWork;
+        }
+
+
+    }
+}
