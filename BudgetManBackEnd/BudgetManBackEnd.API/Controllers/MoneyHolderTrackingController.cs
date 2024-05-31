1a5c7a8397956becae78d1175e746aa5d8a03f4a
maynghien <maynghien@gmail.com>
2024-05-31 10:00:14 +0700

    sdfsd

---
+++ BudgetManBackEnd/BudgetManBackEnd.API/Controllers/MoneyHolderTrackingController.cs
@@ -0,0 +1,26 @@
+﻿using BudgetManBackEnd.Service.Contract;
+using MayNghien.Models.Request.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManBackEnd.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class MoneyHolderTrackingController : Controller
+    {
+        private IAccountBalanceTrackingService _accountBalanceTrackingService;
+        public MoneyHolderTrackingController(IAccountBalanceTrackingService service)
+        {
+            _accountBalanceTrackingService = service;
+        }
+        [HttpPost]
+        [Route("Search")]
+        public IActionResult Search(SearchRequest request)
+        {
+            var result = _accountBalanceTrackingService.Search(request);
+            return Ok(result);
+        }
+    }
+}
