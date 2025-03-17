0a80671ec215bcdb2a0928a4522501d02dfa8d28
maynghien <maynghien@gmail.com>
2025-03-17 14:06:15 +0700

    fix bugs

--- BudgetManBackEnd/BudgetManBackEnd.API/Controllers/BotFrameworkController.cs
+++ BudgetManBackEnd/BudgetManBackEnd.API/Controllers/BotFrameworkController.cs
@@ -3,12 +3,11 @@ using BudgetManBackEnd.Service.Contract;
 using BudgetManBackEnd.Service.Implementation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using BudgetManBackEnd.MessageCronJob;
+using BudgetManBackEnd.BotFramework;
 using MayNghien.Models.Request.Base;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
 namespace BudgetManBackEnd.API.Controllers
@@ -16,25 +15,21 @@ namespace BudgetManBackEnd.API.Controllers
     [Route("api/BotFramework")]
     [ApiController]
     //[Authorize(AuthenticationSchemes = "Bearer")]
-    public class BotFrameworkController : Controller, ActivityHandler
+    public class BotFrameworkController : ControllerBase
     {
         private readonly IBotFrameworkHttpAdapter _adapter;
         private readonly IBot _bot;
-        //private ISkype _loanService;
-        //private MyBot myBot;
+
         public BotFrameworkController(IBotFrameworkHttpAdapter adapter, IBot bot)
         {
             _adapter = adapter;
             _bot = bot;
-            //myBot = new MyBot();
         }
 
         [HttpPost]
         public async Task PostAsync()
         {
-            //await myBot.ProcessAsync(Request, Response);
-            _adapter.ProcessAsync(Request, Response, _bot);
+            await _adapter.ProcessAsync(Request, Response, _bot);
         }
-
     }
 }
